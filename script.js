6e38e83f3fe3db0a36fca7012964c1d825c2f908
CaioPires92 <caiogodoi_92@hotmail.com>
2023-03-30 18:15:48 -0300

    implemented click function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const plates = document.querySelectorAll('.plate')
 const drinks = document.querySelectorAll('.drink')
 const desserts = document.querySelectorAll('.dessert')
+const button = document.querySelector('button')
 
 let pratoEscolhido
 let bebidaEscolhida
@@ -9,6 +10,8 @@ let sobremesaEscolhida
 let pratoEscolhidoValor
 let bebidaEscolhidaValor
 let sobremesaEscolhidaValor
+let contaBorda = 0
+let soma
 
 plates.forEach(function (plate) {
   plate.addEventListener('click', () => {
@@ -20,6 +23,10 @@ plates.forEach(function (plate) {
     if (plate.classList.contains('border')) {
       pratoEscolhido = plate.querySelector('h2').textContent
       pratoEscolhidoValor = plate.querySelector('h3').textContent
+      contaBorda++
+      if (contaBorda === 3) {
+        abilitaBotao()
+      }
     }
   })
 })
@@ -34,6 +41,10 @@ drinks.forEach(function (drink) {
     if (drink.classList.contains('border')) {
       bebidaEscolhida = drink.querySelector('h2').textContent
       bebidaEscolhidaValor = drink.querySelector('h3').textContent
+      contaBorda++
+      if (contaBorda === 3) {
+        abilitaBotao()
+      }
     }
   })
 })
@@ -48,11 +59,15 @@ desserts.forEach(function (dessert) {
     if (dessert.classList.contains('border')) {
       sobremesaEscolhida = dessert.querySelector('h2').textContent
       sobremesaEscolhidaValor = dessert.querySelector('h3').textContent
+      contaBorda++
+      if (contaBorda === 3) {
+        abilitaBotao()
+      }
     }
   })
 })
 
-function concatenarPedidos() {
+function convertNumbers() {
   let new_value_plate = Number(
     pratoEscolhidoValor.replace('R$ ', '').replace(',', '.')
   )
@@ -64,14 +79,27 @@ function concatenarPedidos() {
   let new_value_dessert = Number(
     sobremesaEscolhidaValor.replace('R$ ', '').replace(',', '.')
   )
+  return (soma = new_value_plate + new_value_drink + new_value_dessert)
+}
 
-  let soma = new_value_plate + new_value_drink + new_value_dessert
+function abilitaBotao() {
+  button.removeAttribute('disabled')
+  button.classList.add('active')
+  button.textContent = 'Fechar Pedido'
+}
+
+function concatenarPedidos() {
+  convertNumbers()
 
   let texto = `Olá, gostaria de fazer o pedido:
-  - Prato: ${pratoEscolhido}
-  - Bebida: ${bebidaEscolhida}
-  - Sobremesa: ${sobremesaEscolhida}
-    Total: R$ ${soma.toFixed(2)}`
+    - Prato: ${pratoEscolhido}
+    - Bebida: ${bebidaEscolhida}
+    - Sobremesa: ${sobremesaEscolhida}
+      Total: R$ ${soma.toFixed(2)}`
+
+  console.log(contaBorda)
+
+  // window.open(`https://wa.me/19999999999?text=${texto}`)
 
   console.log(texto)
 }
